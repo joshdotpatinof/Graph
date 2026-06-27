6fb9649da8d517d18ead3a745482184035f547a6
hsojfp <josh.patinof@gmail.com>
2026-06-27 14:01:00 +0800

    Initial commit of HTML elements in Canvas.svelte

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -1,1 +1,9 @@
-<p>This is the canvas!</p>
+<script lang="ts">
+
+</script>
+
+<div class="">
+    <button class="text-white">Config</button>
+    <button class="text-white">Zoom In</button>
+    <button class="text-white">Zoom Out</button>
+</div>
